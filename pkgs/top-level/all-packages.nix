3fe2bbd6c157069c6b9381054d4b2bec1e318cdf
lveneris <tyler@kludgecs.com>
2026-03-10 15:44:13 +0000

    chromium, ungoogled-chromium: introduce variant-aware packaging structure
    Co-authored-by: AsterisMono <noa@requiem.garden>

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -9580,7 +9580,7 @@ with pkgs;
   ungoogled-chromium = callPackage ../applications/networking/browsers/chromium (
     (config.chromium or { })
     // {
-      ungoogled = true;
+      variant = "ungoogled";
     }
   );
 
